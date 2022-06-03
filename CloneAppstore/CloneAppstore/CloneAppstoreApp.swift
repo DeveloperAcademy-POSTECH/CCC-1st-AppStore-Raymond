7a350480fa505be41b8b6619fa643407a50d4d40
SH0123 <sanghyoboy@naver.com>
2022-06-04 00:33:32 +0900

    project setting

---
+++ CloneAppstore/CloneAppstore/CloneAppstoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloneAppstoreApp.swift
+//  CloneAppstore
+//
+//  Created by sanghyo on 2022/06/04.
+//
+
+import SwiftUI
+
+@main
+struct CloneAppstoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
