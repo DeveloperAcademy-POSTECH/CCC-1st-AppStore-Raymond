535e0bc2ed68ab9e0e413ed549ef5df2f586804f
SH0123 <sanghyoboy@naver.com>
2022-06-05 19:18:13 +0900

    feature: NavigationBar body: today view safe area igore 처리, 다른 view들에서 navigation 각각 조건에 맞는 처리 해줄 필요 있음 Resolve : #2

--- CloneAppstore/CloneAppstore/TodayView.swift
+++ CloneAppstore/CloneAppstore/TodayView.swift
@@ -9,7 +9,25 @@ import SwiftUI
 
 struct TodayView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ScrollView {
+                //safe area ignore 되는 문제 발생
+                header
+            }
+            .navigationBarHidden(true)
+        }
+    }
+    
+    var header: some View {
+        VStack(alignment: .leading, spacing: 0) {
+            Text("날짜")
+            HStack {
+                Text("투데이").font(.largeTitle)
+                Spacer()
+                CircleImage()
+            }
+        }
+        .padding(.horizontal)
     }
 }
 
